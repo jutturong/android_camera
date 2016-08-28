dc4d5c2ee01b0d5a9f943a852db45b6b02e88e82
linux <jutturongcharoenrit>
2016-08-29 06:52:03 +0700

    1

---
+++ MyApplication/app/src/main/java/com/example/linux/myapplication/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.linux.myapplication;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ImageView imgView;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+//*** ImageView
+        imgView = (ImageView) findViewById(R.id.imgView);
+
+        //*** Take Photo
+        final Button btnTakePhoto = (Button) findViewById(R.id.btnTakePhoto);
+        // Perform action on click
+        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imgView.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+
+
+}
